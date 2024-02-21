60f0e89e979cd47206e9a7ef43c0afa9b0838e19
alexandru.uioreanu <alexandru.uioreanu@accesa.eu>
2024-02-21 12:43:51 +0200

    fix for overlaying wrong of the border of the MovieCard

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -35,18 +35,20 @@ export const MovieCard: React.FC<{
           Must Watch
         </div>
       )}
-      <div
-        style={{
-          position: "absolute",
-          top: 0,
-          left: 0,
-          right: 0,
-          bottom: 0,
-          border: "5px solid orange",
-          borderRadius: "10px",
-          pointerEvents: "none",
-        }}
-      />
+      {isFavorites && (
+        <div
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            border: "5px solid orange",
+            borderRadius: "10px",
+            pointerEvents: "none",
+          }}
+        />
+      )}
       <div
         style={{
           position: "absolute",
