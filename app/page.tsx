4f487176064ae3ffe35fa801c6adadcb8d44d852
alexandru.uioreanu <alexandru.uioreanu@accesa.eu>
2024-03-11 14:34:02 +0200

    Fixed the issue of redirecting and the vercel problem to proper be deployed

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
-import Image from "next/image";
-import Form from "./register/form";
+import Form from "./login/form";
 import { getServerSession } from "next-auth/next";
-import { redirect } from "next/navigation";
 
 export default async function Home() {
   const session = await getServerSession();
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,13 +1,12 @@
 import { getServerSession } from "next-auth";
 import Form from "./form";
-import { redirect, useRouter } from "next/navigation";
+import { redirect } from "next/navigation";
 
 export default async function RegisterPage() {
   const session = await getServerSession();
-  // const router = useRouter();
 
   if (session) {
-    // router.push("/dashboard");
+    redirect("/dashboard");
   }
   return <Form />;
 }
