57a16266822e32b87f044bc198ab4d8136ac2b4b
alexandru.uioreanu <alexandru.uioreanu@accesa.eu>
2024-03-16 18:36:50 +0200

    Revert "Trying to configure the SSR"
    This reverts commit e8bd221caf6270df0f5b8857dd7f32f75ce37448.

---
+++ app/dashboard/dashboard.tsx
@@ -0,0 +1,21 @@
+export default function Dashboard() {
+  return (
+    <div
+      className="flex flex-col justify-center items-center h-screen w-screen"
+      style={{
+        backgroundImage: 'url("/backgrounddoodleextended.png")',
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        width: "100%",
+        height: "100%",
+      }}
+    >
+      <h1>Dashboard</h1>
+      <h1>Dashboard</h1>
+      <h1>Dashboard</h1>
+      <h1>Dashboard</h1>
+      <h1>Dashboard</h1>
+      <h1>Dashboard</h1>
+    </div>
+  );
+}
