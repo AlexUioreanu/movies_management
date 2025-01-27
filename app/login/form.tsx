1b54f23b0668337ed80c41aafa7421d19baeb8e7
alexandru.uioreanu <alexandru.uioreanu@accesa.eu>
2025-01-27 11:42:41 +0200

    Change the email field to accept more than a email

--- app/login/form.tsx
+++ app/login/form.tsx
@@ -73,7 +73,6 @@ export default function Form() {
         <OutlinedTextField
           name="email"
           label="Email"
-          type="email"
           onChange={handleChange}
           value={inputs.email}
           required
